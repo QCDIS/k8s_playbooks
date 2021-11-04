5807d625df8edc4113c8e4060a47710d0be9f6ba
skoulouzis <koulouzis@gmail.com>
2021-11-04 20:45:24 +0100

    bug fix #9

--- get_service_ports.py
+++ get_service_ports.py
@@ -18,17 +18,17 @@ if __name__ == '__main__':
             port_infos = []
             for port in pp['spec']['ports']:
                 port_type_lower_first = port_type[0].lower() + port_type[1:]
-                url = 'https://' + public_ip + ':' + str(port[port_type_lower_first])
-                url_entry = {'url':url}
+                url = 'http://' + public_ip + ':' + str(port[port_type_lower_first])
+                url_entry = {'url': url}
                 urls.append(url_entry)
                 if 'name' in port:
                     port_name = port['name']
                 else:
                     port_name = port_type_lower_first
-                port_info = {'name': port_name, 'url': url, 'port':port[port_type_lower_first]}
+                port_info = {'name': port_name, 'url': url, 'port': port[port_type_lower_first]}
                 port_infos.append(port_info)
             service['info'] = port_infos
 
             services_array.append(service)
-    services = {'services':services_array}
+    services = {'services': services_array}
     print(json.dumps(services))
